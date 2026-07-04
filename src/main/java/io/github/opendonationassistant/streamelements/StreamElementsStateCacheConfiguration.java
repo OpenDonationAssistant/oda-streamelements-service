4bad255a9b904f8600ec7532b02ea955538e401d
stCarolas <stcarolas@gmail.com>
2026-07-04 10:39:26 +0300

    change to vanilla infinispan

--- src/main/java/io/github/opendonationassistant/streamelements/StreamElementsStateCacheConfiguration.java
+++ src/main/java/io/github/opendonationassistant/streamelements/StreamElementsStateCacheConfiguration.java
@@ -4,17 +4,45 @@ import io.github.opendonationassistant.SerdeableEntryMarshaller;
 import io.github.opendonationassistant.streamelements.repository.StreamElementsData;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.annotation.Value;
 import jakarta.inject.Singleton;
 import java.util.HashMap;
 import java.util.Map;
 import org.infinispan.client.hotrod.DataFormat;
 import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.Configuration;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.commons.marshall.UTF8StringMarshaller;
 
 @Factory
 public class StreamElementsStateCacheConfiguration {
 
   private static final String CACHE_NAME = "streamelements";
+
+  @Singleton
+  public Configuration configuration(
+    @Value("${infinispan.client.hotrod.server.host}") String host,
+    @Value("${infinispan.client.hotrod.server.port}") int port,
+    @Value("${infinispan.client.hotrod.security.username}") String username,
+    @Value("${infinispan.client.hotrod.security.password}") String password
+  ) {
+    return new ConfigurationBuilder()
+      .addServer()
+      .host("localhost")
+      .port(port)
+      .security()
+      .authentication()
+      .username(username)
+      .password(password)
+      .build();
+  }
+
+  @Singleton
+  public RemoteCacheManager cacheManager(Configuration configuration) {
+    var manager = new RemoteCacheManager(configuration);
+    manager.start();
+    return manager;
+  }
 
   @Singleton
   @Requires(env = "standalone")
