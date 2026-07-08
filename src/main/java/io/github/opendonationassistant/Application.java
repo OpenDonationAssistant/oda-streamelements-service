35e1751936de600bff93f5781fbc36d99c188cd5
stCarolas <stcarolas@gmail.com>
2026-07-08 11:18:04 +0300

    use local cache for a while

--- src/main/java/io/github/opendonationassistant/Application.java
+++ src/main/java/io/github/opendonationassistant/Application.java
@@ -5,6 +5,7 @@ import io.github.opendonationassistant.rabbit.Exchange;
 import io.github.opendonationassistant.rabbit.RabbitClient;
 import io.github.opendonationassistant.streamelements.listener.WidgetChangesEventListener;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.rabbitmq.connect.ChannelInitializer;
 import io.micronaut.rabbitmq.connect.ChannelPool;
 import io.micronaut.runtime.Micronaut;
@@ -14,6 +15,11 @@ import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
 import java.util.ArrayList;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
 
 @Factory
 @OpenAPIDefinition(info = @Info(title = "oda-streamelements-service"))
@@ -22,7 +28,46 @@ public class Application {
   public static void main(String[] args) {
     Micronaut.build(args)
       .defaultEnvironments("standalone")
-      .mainClass(Application.class).banner(false).start();
+      .mainClass(Application.class)
+      .banner(false)
+      .start();
+  }
+
+  @Singleton
+  public RemoteCacheManager remoteCacheManager(
+    @Value("${infinispan.client.hotrod.server.host}") String host,
+    @Value("${infinispan.client.hotrod.server.port}") int port,
+    @Value(
+      "${infinispan.client.hotrod.security.authentication.username}"
+    ) String username,
+    @Value(
+      "${infinispan.client.hotrod.security.authentication.password}"
+    ) String password
+  ) {
+    var conf = new ConfigurationBuilder()
+      .addServer()
+      .host(host)
+      .port(port)
+      .security()
+      .authentication()
+      .username(username)
+      .password(password)
+      .build();
+    var manager = new RemoteCacheManager(conf);
+    manager.start();
+    return manager;
+  }
+
+  @Singleton
+  public EmbeddedCacheManager embeddedCacheManager() {
+    // prettier-ignore ON
+    var configuration = new GlobalConfigurationBuilder()
+      .nonClusteredDefault()
+      .build();
+    // prettier-ignore OFF
+    var manager = new DefaultCacheManager(configuration);
+    manager.start();
+    return manager;
   }
 
   @Singleton
--- src/main/java/io/github/opendonationassistant/streamelements/StreamElementsStateCacheConfiguration.java
+++ src/main/java/io/github/opendonationassistant/streamelements/StreamElementsStateCacheConfiguration.java
@@ -1,18 +1,12 @@
 package io.github.opendonationassistant.streamelements;
 
-import io.github.opendonationassistant.SerdeableEntryMarshaller;
 import io.github.opendonationassistant.streamelements.repository.StreamElementsData;
 import io.micronaut.context.annotation.Factory;
-import io.micronaut.context.annotation.Requires;
-import io.micronaut.context.annotation.Value;
 import jakarta.inject.Singleton;
-import java.util.HashMap;
 import java.util.Map;
-import org.infinispan.client.hotrod.DataFormat;
-import org.infinispan.client.hotrod.RemoteCacheManager;
-import org.infinispan.client.hotrod.configuration.Configuration;
-import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
-import org.infinispan.commons.marshall.UTF8StringMarshaller;
+import org.infinispan.commons.api.CacheContainerAdmin.AdminFlag;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.manager.EmbeddedCacheManager;
 
 @Factory
 public class StreamElementsStateCacheConfiguration {
@@ -20,50 +14,13 @@ public class StreamElementsStateCacheConfiguration {
   private static final String CACHE_NAME = "streamelements";
 
   @Singleton
-  public Configuration configuration(
-    @Value("${infinispan.client.hotrod.server.host}") String host,
-    @Value("${infinispan.client.hotrod.server.port}") int port,
-    @Value("${infinispan.client.hotrod.security.authentication.username}") String username,
-    @Value("${infinispan.client.hotrod.security.authentication.password}") String password
-  ) {
-    return new ConfigurationBuilder()
-      .addServer()
-      .host("localhost")
-      .port(port)
-      .security()
-      .authentication()
-      .username(username)
-      .password(password)
-      .build();
-  }
-
-  @Singleton
-  public RemoteCacheManager cacheManager(Configuration configuration) {
-    var manager = new RemoteCacheManager(configuration);
-    manager.start();
-    return manager;
-  }
-
-  @Singleton
-  @Requires(env = "standalone")
   public Map<String, StreamElementsData> streamelementsCache(
-    RemoteCacheManager cacheManager
+    EmbeddedCacheManager cacheManager
   ) {
+    var configuration = new ConfigurationBuilder().simpleCache(true).build();
     return cacheManager
-      .getCache(CACHE_NAME)
-      .withDataFormat(
-        DataFormat.builder()
-          .keyMarshaller(new UTF8StringMarshaller())
-          .valueMarshaller(
-            new SerdeableEntryMarshaller(StreamElementsData.class)
-          )
-          .build()
-      );
-  }
-
-  @Singleton
-  @Requires(env = "allinone")
-  public Map<String, StreamElementsData> streamelementsCache() {
-    return new HashMap<>();
+      .administration()
+      .withFlags(AdminFlag.VOLATILE)
+      .getOrCreateCache(CACHE_NAME, configuration);
   }
 }
